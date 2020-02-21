12ea15f53ddd1e4241701c8d46ff61497f934cd2
Lydeon <leonlee190@gmail.com>
2020-02-21 11:59:23 -0800

    not too sure what i did

--- popup.js
+++ popup.js
@@ -2,9 +2,3 @@
 //     console = chrome.extension.getBackgroundPage().console;
 //     console.log("File name with local path ", downloadItem.filename)
 // });
-
-document.addEventListener('DOMContentLoaded', function () {
-    restoreOptions();
-    document.getElementById("boxed").addEventListener('click', runTimer);
-    console.log("DOM Loaded");
-});
