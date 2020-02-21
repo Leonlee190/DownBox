14f44e77dfac571419cfd7b04c98309977832b9a
Lydeon <leonlee190@gmail.com>
2020-02-20 21:47:16 -0800

    overwrite for same name downloads

---
+++ popup.js
@@ -0,0 +1,4 @@
+// chrome.downloads.onCreated.addListener(function(downloadItem) {
+//     console = chrome.extension.getBackgroundPage().console;
+//     console.log("File name with local path ", downloadItem.filename)
+// });
